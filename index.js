4b85cb5e94d4ee99785f66e1e9ef3910b2366dec
naveen <naveen@perfios.com>
2020-08-12 20:32:33 +0530

    Front-end implementation of push notification.

---
+++ index.js
@@ -0,0 +1,42 @@
+const check = () => {
+    if(!('serviceWorker' in navigator)) {
+        throw new Error('No Service worker support.');
+    }
+
+    if(!('PushManager' in window)) {
+        throw new Error('No Push API support.');
+    }
+}
+
+// Register service worker
+const registerServiceWorker = async () => {
+    const swRegistration = await navigator.serviceWorker.register('service.js');
+    return swRegistration;
+}
+
+const requestNotificationPermission = async () => {
+    const permission = await window.Notification.requestPermission();
+    // value of permission can be 'granted', 'default', 'denied'
+    // granted: user has accepted the request
+    // default: user has dismissed the notification permission popup by clicking on x
+    // denied: user has denied the request.
+    if(permission !== 'granted') {
+        throw new Error('Permission not granted for Notification');
+    }
+}
+
+const showLocalNotification = (title, body, swRegistration) => {
+    const options = {
+        body
+    }
+    swRegistration.showNotification(title, options);
+}
+
+const main = async () => {
+    check();
+    const swRegistration = await registerServiceWorker();
+    const permission = await requestNotificationPermission();
+    showLocalNotification('Push Notification', 'My first push notification', swRegistration);
+}
+
+// main();
